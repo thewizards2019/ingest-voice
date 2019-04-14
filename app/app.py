ef9b6addc3ed7a631e9bee3400882a4868b8a768
dhughes <dhughes2@statestreet.com>
2019-04-14 02:14:24 +0100

    update

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 import speech_recognition as sr
+
 # create_app wraps the other functions to set up the project
+
 
 def create_app(config=None, testing=False, cli=True):
     """
@@ -18,13 +20,13 @@ def create_app(config=None, testing=False, cli=True):
 
         try:
             print("You said " + r.recognize_google(audio))
-            # TODO: add producer {'content': msg}  to kafka topic same as ingest-twitter
+            # TODO: add producer {'content': msg}
+            # to kafka topic same as ingest-twitter
         except sr.UnknownValueError:
             print("Could not understand audio")
         except sr.RequestError as e:
             print("Could not request results; {0}".format(e))
-        
+
         return "Done"
-    
 
     return app
